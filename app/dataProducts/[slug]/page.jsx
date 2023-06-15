18a43810e51d06f1811c9a527291c73808c3b0bf
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2023-06-15 09:13:04 +0400

    fix links15

--- app/dataProducts/[slug]/page.jsx
+++ app/dataProducts/[slug]/page.jsx
@@ -2,7 +2,7 @@
 import {useEffect, useState} from 'react';
 import Image from "next/image";
 import { useRouter } from 'next/router'
-import Modal from "../Component/Modal"
+import Modal from "../../Component/Modal"
 
 
 const Search = () => {
