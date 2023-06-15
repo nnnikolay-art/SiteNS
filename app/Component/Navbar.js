9d109d7369b867a4fe83f3f8baa405304496eddf
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2023-06-15 15:56:43 +0400

    fix logo3

--- app/Component/Navbar.js
+++ app/Component/Navbar.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import logo from '../../public/Logo.jpg'
+import logo2 from '../../public/Logo2.jpg'
 import Image from 'next/image'
 
 const Navbar = () => {
@@ -8,7 +8,7 @@ const Navbar = () => {
             <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <Link href="#" className="flex items-center">
-                        <Image src={logo} className="h-8 mr-3" alt="Flowbite Logo" width={200} height={50}/>
+                        <Image src={logo2} alt="Flowbite Logo" width={200} height={50}/>
                         <span
                             className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ЦентрМедПрокат</span>
                     </Link>
