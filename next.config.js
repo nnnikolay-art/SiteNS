cafb94a7a52af0bcbe24a44a0bdfed143fc24e1f
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2023-06-15 09:11:20 +0400

    fix links14

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
+/**
     output: 'export',
     experimental: {
         appDir: true,
     },
+ */
 }
 module.exports = nextConfig
 
