fb123f9c3b2b926c6e4c2aac8ffe46cbe8635f5d
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2023-06-18 22:25:50 +0400

    fix meta4

--- app/Component/Modal.js
+++ app/Component/Modal.js
@@ -1,93 +1,73 @@
-import ReactModal from 'react-modal';
 import Image from "next/image";
+import Modals from '@mui/material/Modal';
+import {Box, Button, Typography} from "@mui/material";
+
+
+
+const style = {
+    position: 'absolute',
+
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 const Modal = ({ isOpen, handleClose, children, itemModal }) => {
     console.log({itemModal})
     return (
 
-        <ReactModal id="defaultModal"
-             tabIndex="-1"
-             aria-hidden="true"
-             isOpen={isOpen}
-             onRequestClose={handleClose}
-             ariaHideApp={false}
-             contentLabel="Модальное окно"
-             overlayClassName="fixed inset-0 bg-gray-500 bg-opacity-75  flex justify-center items-center"
+        <div>
+            <Button onClick={handleClose}>Open modal</Button>
+            <Modals
+                open={isOpen}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
 
-                    className="mx-auto my-10 p-6 rounded-md bg-white shadow-lg">
-            <div className="relative w-full max-w-2xl max-h-full">
+            >
+                <Box sx={style}>
+                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                         xmlns="http://www.w3.org/2000/svg"
+                         onClick={() => handleClose(true)}
+                             >
+                        <path fill-rule="evenodd"
+                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              clip-rule="evenodd"></path>
+                    </svg>
 
-                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        {itemModal.title}
+                    </Typography>
+                    <Image src={"/ProductImg/" + itemModal.img} width={200} height={200}
+                           className="w-full"
+                    />
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        {itemModal.desc}
+                        {itemModal.price && <p className="font-bold text-blue-700">Продажа: {itemModal.price} р. </p>}
+                        {itemModal.priceA && <p className="font-bold text-blue-800">Аренда: {itemModal.priceA}</p>}
+                    </Typography>
+                    <div className="border-1"/>
+                    <Typography id="modal-modal-description" sx={{ px: 4, mt: 2, maxHeight: "50vh", overflowY: "auto" }} >
+                        {itemModal.longDesc}
 
-                    <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                            {itemModal.title}
-                            <Image src={"/ProductImg/" + itemModal.img} width={200} height={200}
-                                   className="w-full"
-                            />
-                        </h3>
-
-                        <button type="button"
+                        <Button type="button"
                                 className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                                 data-modal-hide="defaultModal"
-                                 onClick={() => handleClose(true)}   >
-                            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                 xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                      clip-rule="evenodd"></path>
-                            </svg>
-                            <span className="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    {children}
-                    <div className="p-6 space-y-6">
-                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            {itemModal.desc}
-                        </p>
-                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            {itemModal.longDesc}
-                        </p>
-                    </div>
-
-                    <div
-                        className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                        <button data-modal-hide="defaultModal" type="button"
-                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                onClick={() => handleClose(true)}
+                        onClick={() => handleClose(true) }
                         >
-                            Закрыть
-                        </button>
 
-                    </div>
-                </div>
-            </div>
-        </ReactModal>
-
-
-        /*
-        <ReactModal
-            isOpen={isOpen}
-            onRequestClose={handleClose}
-            ariaHideApp={false}
-            contentLabel="Модальное окно"
-            className="mx-auto my-10 p-6 rounded-md bg-white shadow-lg"
-            overlayClassName="fixed inset-0 bg-gray-500 bg-opacity-75  flex justify-center items-center"
-        >
-            {children}
-            <Image src={"/ProductImg/" + itemModal.img} width={200} height={200}
-                className=""
-            />
-            <h1 className="font-bold">{itemModal.title}</h1>
-            <h2>
-            {itemModal.desc}
-            </h2>
-            <h3>
-            {itemModal.longDesc}
-            </h3>
-
-        </ReactModal>
-        */
+                            <span className="sr-only">Закрыть</span>
+                        </Button>
+                    </Typography>
+                </Box>
+            </Modals>
+        </div>
     );
 };
 
