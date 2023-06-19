6f51817b3f23c58087d07093db3fe251513f9886
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2023-06-19 09:30:08 +0400

    fix cls4

--- app/contact/page.js
+++ app/contact/page.js
@@ -43,7 +43,7 @@ const ContactList = () => (
                         <Image className="float-left" src={addr} alt='Адрес:' width={25} height={25}/>
                         <p className="text-gray-700 mb-1">Адрес: {address}</p>
                         <Image className="float-left" src={PhoneIcon} alt='Телефон:' width={25} height={25}/>
-                        <Link className="text-gray-700 mb-1">Телефон: {phone}</Link>
+                        <Link href="tel:" className="text-gray-700 mb-1">{phone}</Link>
                         <Image className="float-left" src={MailIcon} alt='Email:' width={25} height={25}/>
                         <p className="text-gray-700">Email: {email}</p>
                 </div>
