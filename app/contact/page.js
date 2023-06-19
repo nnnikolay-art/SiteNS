c7e055e30bd619dd7e5ea706216f7e54fbabbdee
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2023-06-19 10:18:39 +0400

    fix cls12

--- app/contact/page.js
+++ app/contact/page.js
@@ -44,11 +44,11 @@ const ContactList = () => (
                         <p className="text-gray-700 mb-1">Адрес: {address}</p>
                         <div className="grid-rows-1">
                             <Image className="float-left" src={PhoneIcon} alt='Телефон:' width={25} height={25}/>
-                            <Link href={"tel:"+phone} className="text-gray-700 mb-1">Телефон: {phone}</Link>
+                            <Link href={"tel:"+phone} className="text-gray-700 mb-1">{phone}</Link>
                         </div>
                         <div className="grid-rows-1">
-                            <Image className="float-left" src={MailIcon} alt='Email:' width={25} height={25}/>
-                            <Link href={"mailto:"+email} className="text-gray-700 mb-1">Почта: {email}</Link>
+                            <Image className="float-left mx-0 flex-" src={MailIcon} alt='Email:' width={25} height={25}/>
+                            <Link href={"mailto:"+email} className="text-gray-700 mb-1">{email}</Link>
                         </div>
 
                 </div>
