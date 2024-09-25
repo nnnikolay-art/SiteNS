80c022f39de064694c538a7bc2410293e5729bee
DESKTOP-I94MI2F\user <iiipoizone@gmail.com>
2024-09-25 14:54:41 +0400

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/app/Component/Navbar'
 import Logo from "@/app/Component/Logo";
 import VK_Logo from '../public/Icons/vk-128x128.png'
+import Instagram_Logo from '../public/Icons/instagramm.png'
 const inter = Inter({ subsets: ['latin'] })
 import Image from 'next/image';
 
@@ -27,7 +28,8 @@ export default function RootLayout({
       {children}
       </body>
       <div className="text-center bg-gray-100 text-black">
-          <div className="">
+
+
               <div>
                   <p className="font-bold text-1xl">Режим работы:</p>
                   <p className="text-1xl">Будни с 9-00 до 18-00</p>
@@ -39,13 +41,16 @@ export default function RootLayout({
                   <p>ИНН: 183313423733, ОГРН: 32183200005883</p>
                   <p className="font-bold">Имеются противопоказания, необходима консультация специалиста</p>
               </div>
-              <br></br>
-              <div className="justify-center justify-items-center flex">
-                  <a href="https://vk.com/medprokatur">
-                      <Image src={VK_Logo} alt="Вконтакте" width={100} height={100}/>
-                  </a>
-              </div>
+
+          <div className="justify-center justify-items-center flex ">
+              <a href="https://vk.com/medprokatur">
+                  <Image src={VK_Logo} alt="Вконтакте" width={100} height={100}/>
+              </a>
+              <a href="https://instagram.com/medprokatur">
+                  <Image src={Instagram_Logo} alt="Инстаграмм" width={100} height={100}/>
+              </a>
           </div>
+              <br></br>
       </div>
     </html>
   )
